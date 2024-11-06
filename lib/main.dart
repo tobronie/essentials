e906abf31fa3cf8648d459c380656999e37b8e76
tobronie <tobronie05@gmail.com>
2024-11-06 14:30:32 +0700

    progres 1

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,13 @@
 import 'package:essentials/firebase_options.dart';
+import 'package:essentials/screens/onboarding_screen.dart';
+import 'package:essentials/theme/theme.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:essentials/screens/spalsh_screen.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
 );
@@ -11,11 +15,15 @@ void main() async {
 }
 
 class MyApp extends StatelessWidget {
+  var auth = FirebaseAuth.instance;
+
   @override
   Widget build(BuildContext context){
     return MaterialApp(
+      title: 'Essentials',
       debugShowCheckedModeBanner: false,
-      home: SplashScreen(),
+      theme: modeGelap ? darkMode : lightMode,
+      home: auth.currentUser != null? SplashScreen() : OnboardingScreen(),
     );
   }
 }
