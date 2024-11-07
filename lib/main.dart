5efc0e0108712c53badb9ef7763e67c0e589f5aa
tobronie <tobronie05@gmail.com>
2024-11-07 19:56:37 +0700

    awal data

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,29 @@
+import 'package:essentials/firebase_options.dart';
+import 'package:essentials/screens/onboarding_screen.dart';
 import 'package:essentials/theme/theme.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:essentials/screens/spalsh_screen.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+);
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  var auth = FirebaseAuth.instance;
+
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context){
     return MaterialApp(
       title: 'Essentials',
       debugShowCheckedModeBanner: false,
       theme: modeGelap ? darkMode : lightMode,
-      home: SplashScreen(),
+      home: auth.currentUser != null? SplashScreen() : OnboardingScreen(),
     );
   }
 }
