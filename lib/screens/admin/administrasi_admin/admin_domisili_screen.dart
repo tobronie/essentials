c465c89eeebee1947c68241a38c3813c8761e349
tobronie <tobronie05@gmail.com>
2025-01-23 19:18:07 +0700

    aaa

--- lib/screens/admin/administrasi_admin/admin_domisili_screen.dart
+++ lib/screens/admin/administrasi_admin/admin_domisili_screen.dart
@@ -27,7 +27,7 @@ class _Admin_DomisiliScreenState extends State<Admin_DomisiliScreen> {
   Future<void> pickDocument() async {
     final FilePickerResult? result = await FilePicker.platform.pickFiles(
       type: FileType.custom,
-      allowedExtensions: ['pdf', 'doc', 'docx'],
+      allowedExtensions: ['pdf'],
     );
 
     if (result != null && result.files.single.path != null) {
